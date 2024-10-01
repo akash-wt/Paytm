5005c860b400d1e597d5497ffde8de53638018d6
Akash <akash906129@gmail.com>
2024-10-01 16:57:02 +0530

    added signin and update routes .

--- Backend/Models/user.js
+++ Backend/Models/user.js
@@ -1,28 +1,35 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true,
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
     },
     firstName: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 50,
     },
     lastName: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 50,
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
+      minLength: 6,
     },
-},{timestamps: true})
-
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
--- Backend/Routes/user.js
+++ Backend/Routes/user.js
@@ -6,6 +6,7 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const User = require("../Models/user");
 const Account = require("../Models/Balence");
+const authMiddleware=require("../authorization");
 
 const signupSchema = z.object({
   username: z.string().email(),
@@ -14,6 +15,7 @@ const signupSchema = z.object({
   lastName: z.string(),
 });
 
+// signup
 router.post("/signup", async (req, res) => {
   const { success, data, error } = signupSchema.safeParse(req.body);
   if (!success) {
@@ -29,7 +31,7 @@ router.post("/signup", async (req, res) => {
     });
 
     if (userExist) {
-      return res.status(400).json({
+      return res.status(411).json({
         message: "Email already exists",
       });
     }
@@ -37,7 +39,6 @@ router.post("/signup", async (req, res) => {
     const saltRounds = 10;
 
     const hashedPassword = await bcrypt.hash(data.password, saltRounds);
-
 
     const newUser = new User({ ...data, password: hashedPassword });
 
@@ -50,6 +51,7 @@ router.post("/signup", async (req, res) => {
     const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, {
       expiresIn: "21d",
     });
+
     return res.status(200).json({
       message: "user created successfully",
       token: token,
@@ -57,6 +59,81 @@ router.post("/signup", async (req, res) => {
   } catch (e) {
     res.status(500).json({ message: " enternal sever error", error: e });
   }
+});
+
+// signin
+
+const signInSchema = z.object({
+  username: z.string().email(),
+  password: z.string(),
+});
+
+router.post("/signin", async (req, res) => {
+  const { success, data, error } = signInSchema.safeParse(req.body);
+  if (!success) {
+    return res.status(411).json({
+      message: "invalid inputs",
+      error: error.errors,
+    });
+  }
+  const user = await User.findOne({ username: data.username });
+  if (!user) {
+    return res.status(411).json({
+      message: "user not found ",
+    });
+  }
+
+  const rightPassword = await bcrypt.compare(data.password, user.password);
+  if(!rightPassword){
+    console.log("password ", rightPassword);
+    return res.json({
+      message:"wrong password"
+    })
+  }
+
+
+  if (rightPassword) {
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "21d",
+    });
+
+    return res.status(200).json({
+      message: "signin successfully",
+      token: token,
+    });
+  }
+
+  res.status(411).json({
+    message: "Error while logging in",
+  });
+});
+
+// update
+const updateSchema=z.object({
+  password:z.string().min(6).optional(),
+  lastName:z.string().optional(),
+  firstName:z.string().optional()
+})
+router.post("/update", authMiddleware, async (req, res) => {
+
+  console.log(req.body);
+  
+    const {success,data,error}=updateSchema.safeParse(req.body);
+    if(!success){
+      return res.status(411).json({
+        message:"invalid inputs",
+        error:error.errors
+      })
+    }
+
+    const user=await User.findByIdAndUpdate(req.userId,data);
+console.log(user);
+
+
+    res.json({
+      message:"updated successfully"
+    })
+
 });
 
 module.exports = router;
